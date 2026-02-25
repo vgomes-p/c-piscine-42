3783d7b28eda9e555b31c6fa51de19737e042f97
Vinicius Eduardo Gomes Pereira <vigomes-@c3r7p1.42sp.org.br>
2026-02-25 19:52:01 -0300

    fix: fixing tests for ft_print_comb, ft_print_comb2 and pft_putnbr

--- TESTERS/c00/test_ft_putnbr.c
+++ TESTERS/c00/test_ft_putnbr.c
@@ -3,10 +3,10 @@ int main()
 {
 	ft_putnbr(5);
 	ft_putc('\n');
-	ft_putnbr(353);
+	ft_putnbr(-353);
 	ft_putc('\n');
 	ft_putnbr(42);
 	ft_putc('\n');
-	ft_putnbr(124453);
+	ft_putnbr(-124453);
 	return (0);
 }
