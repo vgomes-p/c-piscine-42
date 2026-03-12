645aadfefd4f11d773205cf617ff5c72cb38f252
Vinicius Eduardo Gomes Pereira <vigomes-@c2r1p3.42sp.org.br>
2026-03-11 21:26:10 -0300

    feat: c04 approved 70/100

---
+++ piscine/c04/ex02/ft_putnbr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vigomes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/09 15:21:45 by vigomes-          #+#    #+#             */
+/*   Updated: 2026/03/11 19:18:49 by vigomes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char ch)
+{
+	write(1, &ch, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	long int	n;
+	char		c;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n > 9)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	ft_putchar(c);
+}
+
+// https://onlinegdb.com/0laAXuD0N
